780c65b8eeff34c5ce4082cb4f19dbe570c8ad35
KEVINDUARTES <kevinduartes792@gmail.com>
2023-05-12 01:20:22 -0300

    first commit

---
+++ src/app/data/people.data.ts
@@ -0,0 +1,145 @@
+import { Person } from '../models';
+
+export const People: Person[] = [
+  {
+    id: '1',
+    name: 'John Smith',
+    category: 'manager',
+    company: 'Big Burgers Inc.',
+    levelOfHappiness: '75'
+  },
+  {
+    id: '2',
+    name: 'Jane Doe',
+    category: 'manager',
+    company: 'Taco Palace',
+    levelOfHappiness: '90'
+  },
+  {
+    id: '3',
+    name: 'Bob Johnson',
+    category: 'manager',
+    company: 'Pizza Planet',
+    levelOfHappiness: '60'
+  },
+  {
+    id: '4',
+    name: 'Sarah Lee',
+    category: 'manager',
+    company: 'Waffle House',
+    levelOfHappiness: '80'
+  },
+  {
+    id: '5',
+    name: 'Mike Brown',
+    category: 'manager',
+    company: 'Sushi Time',
+    levelOfHappiness: '40'
+  },
+  {
+    id: '6',
+    name: 'Karen Davis',
+    category: 'manager',
+    company: 'Tasty Treats',
+    levelOfHappiness: '95'
+  },
+  {
+    id: '7',
+    name: 'Tom Wilson',
+    category: 'manager',
+    company: 'Burger Joint',
+    levelOfHappiness: '50'
+  },
+  {
+    id: '8',
+    name: 'Emily Watson',
+    category: 'manager',
+    company: 'Hot Dog Heaven',
+    levelOfHappiness: '30'
+  },
+  {
+    id: '9',
+    name: 'Mark Taylor',
+    category: 'manager',
+    company: 'Italian Delight',
+    levelOfHappiness: '70'
+  },
+  {
+    id: '10',
+    name: 'Melissa Adams',
+    category: 'manager',
+    company: 'BBQ King',
+    levelOfHappiness: '85'
+  },
+  {
+    id: '11',
+    name: 'Jessica Lee',
+    category: 'employee',
+    company: 'Pita Palace',
+    levelOfHappiness: '70'
+  },
+  {
+    id: '12',
+    name: 'James Smith',
+    category: 'employee',
+    company: 'Smoothie Paradise',
+    levelOfHappiness: '45'
+  },
+  {
+    id: '13',
+    name: 'Megan Davis',
+    category: 'employee',
+    company: 'Ice Cream Dream',
+    levelOfHappiness: '90'
+  },
+  {
+    id: '14',
+    name: 'Andrew Johnson',
+    category: 'employee',
+    company: 'Seafood Haven',
+    levelOfHappiness: '60'
+  },
+  {
+    id: '15',
+    name: 'Rachel Brown',
+    category: 'employee',
+    company: 'Veggie Delight',
+    levelOfHappiness: '75'
+  },
+  {
+    id: '16',
+    name: 'Kevin Davis',
+    category: 'employee',
+    company: 'Bakery Bliss',
+    levelOfHappiness: '55'
+  },
+  {
+    id: '17',
+    name: 'Linda Wilson',
+    category: 'employee',
+    company: 'Bistro Cafe',
+    levelOfHappiness: '50'
+  },
+  {
+    id: '18',
+    name: 'Chris Taylor',
+    category: 'employee',
+    company: 'Smoothie Paradise',
+    levelOfHappiness: '80'
+  },
+
+  {
+    id: '19',
+    name: 'Samad Emery',
+    category: 'employee',
+    company: 'My Hair Lady',
+    levelOfHappiness: '35'
+  },
+  {
+    id: '20',
+    name: 'Ellouise Hammond',
+    category: 'employee',
+    company: 'The Director’s Cut',
+    levelOfHappiness: '56'
+  }
+];
